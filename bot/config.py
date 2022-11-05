f52a7fcdf00aa7e35881a03a969f0f42854a3024
Keyz400 <109141199+Keyz400@users.noreply.github.com>
2022-11-05 15:22:41 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -16,11 +16,11 @@
 from decouple import config
 
 try:
-    APP_ID = config("APP_ID", cast=int)
-    API_HASH = config("API_HASH")
-    BOT_TOKEN = config("BOT_TOKEN")
+    APP_ID = config("11321821", cast=int)
+    API_HASH = config("bbf63cf902c2548c31963ccd88c4c6f7")
+    BOT_TOKEN = config("5615063323:AAHzWuekv-wIvLHRJo3cOcAhj1daMZxsqEI")
     DEV = 1287276743
-    OWNER = config("OWNER")
+    OWNER = config("5665936025")
     ffmpegcode = ["-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By AnshuSharma (https://github.com/Anshusharma75/TG-videoCompress)' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
     THUMB = config("THUMBNAIL")
 except Exception as e:
